276ab665477f98e25be1744c37ba06fbe8cc1ac6
Syed Muntazir Mehdi <150386702+Muntazir999@users.noreply.github.com>
2024-01-30 21:06:04 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,42 @@
+import inquirer from "inquirer";
+
+let todos: string[] = [];
+let loop = true;
+
+while(loop){
+    const answers : {
+        TODO: string,
+        Another: boolean
+    } = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: "Add your todo"
+        },
+        {
+            type: "confirm",
+            name: "Another",
+            message: "Want to add another ?",
+            default: false
+        }
+    ])
+    const {TODO, Another} = answers;
+    // console.log(answers)
+    loop = Another
+    if(TODO){
+        todos.push(TODO)
+    }else {
+        console.log("Kindly give valid input!!!")
+    }
+}
+
+// console.log(todos)
+
+if(todos.length > 0){
+    console.log("Your Todo List:")
+    todos.forEach(todo => {
+        console.log(todo)
+    })
+}else {
+    console.log("No todos found")
+}
